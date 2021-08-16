dcf4a26a6c00d6dec958a16d3d897aa36d1b670a
Tim Hunter <thunte27@uwo.ca>
2021-08-16 14:11:09 -0400

    Initial Commit

---
+++ model/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  model
+//
+//  Created by Tim  Hunter on 2021-08-16.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
