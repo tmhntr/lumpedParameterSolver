a547b9b644af09eca3670e4a3d562cf629936da1
Tim Hunter <thunte27@uwo.ca>
2021-09-09 11:24:57 -0400

    fixing wrapper

---
+++ src/model/wrapper.cpp
@@ -0,0 +1,127 @@
+#include "wrapper.hpp"
+
+wrapper::wrapper() : model("wrapper") {}
+wrapper::wrapper(std::vector<model *> mdls) : wrapper()
+{
+    for (std::vector<model *>::iterator it = mdls.begin() ; it != mdls.end(); ++it)
+    {
+        addModel(*it);
+    }
+}
+
+std::string wrapper::getDerivedName(int index)
+{ return ""; }
+
+void wrapper::addModel(model * mdl)
+{
+    if(getY()!=NULL)
+    {
+      mdl->setY(getY());
+    }
+    models.push_back(mdl);
+    int oldNeq = getNEQ();
+    setNEQ(oldNeq + mdl->getNEQ());
+    stateNames.resize(getNEQ());
+    for (int i = 0; i < mdl->getNEQ(); i++)
+    {
+        setStateName(i+oldNeq, mdl->getStateName(i));
+    }
+}
+
+model * wrapper::getModel(int index) { return models[index]; }
+model * wrapper::getModel(std::string modelName)
+{
+    std::vector<model *>::iterator it = models.begin();
+    while (it != models.end())
+    {
+        if (modelName == (*it)->getName())
+            return (*it);
+    }
+    return NULL;
+}
+std::vector<model *> wrapper::getModelVec() { return models; }
+
+std::string * wrapper::getStateNames()
+{
+    return stateNames.data();
+}
+
+void wrapper::updateDerived(double t, double y[])
+{
+    for (int i = 0; i <  models.size(); i++)
+    {
+        models[i]->updateDerived(t, y);
+    }
+}
+
+void wrapper::getDY(double t, double y[], double * DY)
+{
+    model * mdlPtr;
+    double * DYPtr = DY;
+    for (std::vector<model *>::iterator it = models.begin() ; it != models.end(); ++it)
+    {
+        mdlPtr = *it;
+//            std::cout << mdlPtr->getName() << std::endl;
+        mdlPtr->getDY(t, y, DYPtr);
+        DYPtr+=mdlPtr->getNEQ();
+    }
+}
+
+int wrapper::init(solver * slvr)
+{
+    slvr->setModel(this);
+    return init(components(), stateNames, slvr);
+}
+
+int init(model * parent)
+{
+    int retval = 0;
+//        std::vector<component *> flatmodlist = flattenModList(modlist);
+    std::vector<model *> modlist = components();
+    for (std::vector<model *>::iterator it = modlist.begin() ; it != modlist.end(); it++)
+    {
+        try {
+            if ((*it)->init(parent) != 0)
+                throw 1;
+        } catch (int e) {
+            std::cout << "Initialization for model (" << (*it)->getName() << ") failed." << std::endl;
+            retval++;
+        }
+    }
+    return retval;
+}
+
+std::vector<component *> flattenModList(std::vector<model *> modlist)
+{
+    std::vector<component *> newModList;
+    wrapper * wrap;
+    for (int i = 0; i < modlist.size(); i++)
+    {
+
+        if (typeid(wrapper) == typeid(*(modlist[i])))
+        {
+            wrap = (wrapper *) modlist[i];
+            std::vector<component *> newNewModList = flattenModList(wrap->getModelVec());
+            for (int j = 0; j  < newNewModList.size(); j++)
+            {
+                newModList.push_back(newNewModList[j]);
+            }
+        } else
+        {
+            newModList.push_back((component *) modlist[i]);
+        }
+    }
+    return newModList;
+}
+
+std::vector<component *> flattenModList() {return flattenModList(models); }
+
+std::vector<model *> components()
+{
+    std::vector<model *> mlist;
+    std::vector<component *> clist = flattenModList(models);
+    for (int i = 0; i < clist.size(); i++) {
+        mlist.push_back((model *) clist[i]);
+    }
+    return mlist;
+}
--- src/model/wrapper.hpp
+++ src/model/wrapper.hpp
@@ -10,7 +10,8 @@
 
 #include <stdio.h>
 #include <iostream>
-#include "interface.hpp"
+#include "solver/solver.hpp"
+#include "model.hpp"
 #include "component.hpp"
 #include <math.h>
 #include <string>
@@ -23,147 +24,30 @@ private:
 public:
 //    std::vector<std::string> stateNames;
 
-    wrapper() : model("wrapper") {}
-    wrapper(std::vector<model *> mdls) : wrapper()
-    {
-        for (std::vector<model *>::iterator it = mdls.begin() ; it != mdls.end(); ++it)
-        {
-            addModel(*it);
-        }
-    }
+    wrapper();
+    wrapper(std::vector<model *> mdls);
+    std::string getDerivedName(int index);
+    void addModel(model * mdl);
 
-    void addModel(model * mdl)
-    {
-        if(getY()!=NULL)
-        {
-          mdl->setY(getY());
-        }
-        models.push_back(mdl);
-        int oldNeq = getNEQ();
-        setNEQ(oldNeq + mdl->getNEQ());
-        stateNames.resize(getNEQ());
-        for (int i = 0; i < mdl->getNEQ(); i++)
-        {
-            setStateName(i+oldNeq, mdl->getStateName(i));
-        }
-    }
+    model * getModel(int index);
+    model * getModel(std::string modelName);
+    std::vector<model *> getModelVec();
 
-    model * getModel(int index) { return models[index]; }
-    model * getModel(std::string modelName)
-    {
-        std::vector<model *>::iterator it = models.begin();
-        while (it != models.end())
-        {
-            if (modelName == (*it)->getName())
-                return (*it);
-        }
-        return NULL;
-    }
-    std::vector<model *> getModelVec() { return models; }
+    std::string * getStateNames();
 
-    std::string * getStateNames()
-    {
-        return stateNames.data();
-    }
+    void updateDerived(double t, double y[]);
 
-//    void updateStateNames()
-//    {
-//        // reallocate statenames so that it fits all names.
-////        if (stateNames)
-////            free(stateNames);
-////        stateNames = (std::string *) calloc(getNEQ(), sizeof(std::string));
-//
-//        std::vector<submodule *>::iterator it =  models.begin();
-//        int c = 0;
-//        while (c < getNEQ() && it != models.end())
-//        {
-//            for (int i = 0; i < (*it)->getNEQ(); i++)
-//            {
-//                stateNames.push_back((*it)->getStateName(i));
-//                c++;
-//            }
-//            it++;
-//        }
-//    }
+    void getDY(double t, double y[], double * DY);
 
-    void updateDerived(double t, double y[])
-    {
-        for (int i = 0; i <  models.size(); i++)
-        {
-            models[i]->updateDerived(t, y);
-        }
-    }
+    int init(solver * slvr);
 
-    void getDY(double t, double y[], double * DY)
-    {
-        model * mdlPtr;
-        double * DYPtr = DY;
-        for (std::vector<model *>::iterator it = models.begin() ; it != models.end(); ++it)
-        {
-            mdlPtr = *it;
-//            std::cout << mdlPtr->getName() << std::endl;
-            mdlPtr->getDY(t, y, DYPtr);
-            DYPtr+=mdlPtr->getNEQ();
-        }
-    }
+    int init(std::vector<model *> modlist, std::vector<std::string> stateVars, solver * slvr);
 
-    int init(solver * slvr)
-    {
-        slvr->setModel(this);
-        return init(components(), stateNames, slvr);
-    }
+    std::vector<component *> flattenModList(std::vector<model *> modlist);
 
-    int init(std::vector<model *> modlist, std::vector<std::string> stateVars, solver * slvr)
-    {
-        int retval = 0;
-//        std::vector<component *> flatmodlist = flattenModList(modlist);
-        for (std::vector<model *>::iterator it = modlist.begin() ; it != modlist.end(); it++)
-        {
-            try {
-                if ((*it)->init(slvr) != 0)
-                    throw 1;
-            } catch (int e) {
-                std::cout << "Initialization for model (" << (*it)->getName() << ") failed." << std::endl;
-                retval++;
-            }
-        }
-        return retval;
-    }
+    std::vector<component *> flattenModList();
 
-    std::vector<component *> flattenModList(std::vector<model *> modlist)
-    {
-        std::vector<component *> newModList;
-        wrapper * wrap;
-        for (int i = 0; i < modlist.size(); i++)
-        {
-
-            if (typeid(wrapper) == typeid(*(modlist[i])))
-            {
-                wrap = (wrapper *) modlist[i];
-                std::vector<component *> newNewModList = flattenModList(wrap->getModelVec());
-                for (int j = 0; j  < newNewModList.size(); j++)
-                {
-                    newModList.push_back(newNewModList[j]);
-                }
-            } else
-            {
-                newModList.push_back((component *) modlist[i]);
-            }
-        }
-        return newModList;
-    }
-
-    std::vector<component *> flattenModList() {return flattenModList(models); }
-
-    std::vector<model *> components()
-    {
-        std::vector<model *> mlist;
-        std::vector<component *> clist = flattenModList(models);
-        for (int i = 0; i < clist.size(); i++) {
-            mlist.push_back((model *) clist[i]);
-        }
-        return mlist;
-    }
+    std::vector<model *> components();
 };
 
 
